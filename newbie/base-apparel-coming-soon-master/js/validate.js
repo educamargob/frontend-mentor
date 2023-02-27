05100766f3b5ed845fe430d521324a88c29bc39f
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2023-02-27 16:26:38 -0300

    Javascript for errors

--- newbie/base-apparel-coming-soon-master/js/validate.js
+++ newbie/base-apparel-coming-soon-master/js/validate.js
@@ -1,19 +1,29 @@
 //Verify if the email is valid
 
 const email = document.querySelector("#email");
+const submitBtn = document.querySelector(".form button");
 const error = document.querySelector('.form-errors');
+const errorIco = document.querySelector('.error-icon');
 
-function eventFocusOut(event) {
-    const email = event.currentTarget;
+
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
     const value = email.value;
     let pattern = /[a-z]@[a-z]/i;
     if(!pattern.test(value)){
         email.style.borderColor = 'var(--c-p1)';
         error.style.display = 'block';
-    } else {
-        email.style.borderColor = 'hsl(348, 16%, 82%)';
-        error.style.display = 'none';
+        error.innerText = "Please provide a valid email";
+        errorIco.style.display = 'block';
+    }else {
+        alert("GOOD JOB!")
     }
-}
+    
+});
 
-email.addEventListener('focusout', eventFocusOut);
+document.getElementById("email").addEventListener("keyup", function () {
+    email.style.borderColor = 'hsl(348, 16%, 82%)';
+    error.style.display = 'none';
+    errorIco.style.display = 'none';
+});
