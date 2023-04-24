4f48b965c430db26a35276ea3ecaadb62cd99dd3
Eduardo Camargo Ribeiro Borges <eduardo.borges@adami.corporativo>
2023-04-24 14:11:08 -0300

    thanks page

--- advanced/multi-step-form-main/js/multi-step-form.js
+++ advanced/multi-step-form-main/js/multi-step-form.js
@@ -7,24 +7,32 @@ showTab(currentTab);
 
 function showTab(tab) {
   //show the called tab
-  var nextBtn = document.querySelector("#nextBtn");
-  tabs[tab].classList.add('active');
-  barItemSelected(tab);
-  if (tab == 0){
-    document.querySelector("#backBtn").style.display = "none";
-    document.querySelector(".buttons").style.justifyContent = "end";
-  } else{
-    document.querySelector(".buttons").style.justifyContent = "space-between";
-    document.querySelector("#backBtn").style.display = "block";
-  }
-  
-  if (tab == (tabs.length - 1)){
-    getValues();
-    nextBtn.innerHTML = "Confirm";
-    nextBtn.classList.add("c-2")
-  } else {
-    nextBtn.innerHTML = "Next Step";
-    nextBtn.classList.remove("c-2")
+  if(tab == 4){
+    var form = document.querySelector('form');
+    var thanks = document.querySelector('.thanks-page');
+    form.style.display = 'none';
+    thanks.style.display = 'flex';
+
+  }else {
+    var nextBtn = document.querySelector("#nextBtn");
+    tabs[tab].classList.add('active');
+    barItemSelected(tab);
+    if (tab == 0){
+      document.querySelector("#backBtn").style.display = "none";
+      document.querySelector(".buttons").style.justifyContent = "end";
+    } else{
+      document.querySelector(".buttons").style.justifyContent = "space-between";
+      document.querySelector("#backBtn").style.display = "block";
+    }
+    
+    if (tab == (tabs.length - 1)){
+      getValues();
+      nextBtn.innerHTML = "Confirm";
+      nextBtn.classList.add("c-2");
+    } else {
+      nextBtn.innerHTML = "Next Step";
+      nextBtn.classList.remove("c-2");
+    }
   }
 }
 
